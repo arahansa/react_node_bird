5f13f9163dd1b41c914f762e098e499f3a29a6f4
arahansa <arahansa@naver.com>
2020-09-30 13:29:28 +0900

    nodebird 1-4 andt apply

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
+import AppLayout from "../components/AppLayout";
+import Head from 'next/head';
 
 const Home = () => {
     return (
         <>
-            <Link href="/about"><a>about</a></Link>
-            <div>Hello, Next</div>
+            <Head>
+                <title>NodeBird</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.6.6/antd.min.css"/>
+            </Head>
+            <AppLayout>
+                <Link href="/about"><a>about</a></Link>
+                <div>Hello, Next</div>
+            </AppLayout>
         </>
     );
 };
