79ce21d25a7a0f8a818e3fc5dab16104d911f885
arahansa <arahansa@naver.com>
2020-09-30 15:41:41 +0900

    1-7 form submit console

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -9,8 +9,21 @@ const Signup = () => {
     const [password, setPassword] = useState('');
     const [passwordCheck, setPasswordCheck] = useState('');
     const [term, setTerm] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [termError, setTermError] = useState(false);
 
-    const onSubmit = () => {};
+    const onSubmit = (e) => {
+
+        if(password !== passwordCheck){
+            return setPasswordError(true);
+        }
+        if(!term){
+            return setTermError(true);
+        }
+        console.log({
+            id,nick,password,passwordCheck,term
+        });
+    };
     const onChangeId = (e) => {
         setId(e.target.value);
     };
@@ -21,10 +34,12 @@ const Signup = () => {
         setPassword(e.target.value);
     };
     const onChangePasswordCheck = (e) => {
+        setPasswordError(e.target.value !== password);
         setPasswordCheck(e.target.value);
     };
     const onChangeTerm = (e) => {
-        setTerm(e.target.value);
+        setTermError(false);
+        setTerm(e.target.checked);
     };
 
     return (
@@ -34,7 +49,7 @@ const Signup = () => {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.6.6/antd.min.css"/>
             </Head>
             <AppLayout>
-                <Form onSubmit={onSubmit} style={{padding: 10}}>
+                <Form onFinish={onSubmit} style={{padding: 10}}>
                     <div>
                         <label htmlFor="user-id">아이디</label>
                         <br/>
@@ -54,11 +69,13 @@ const Signup = () => {
                         <label htmlFor="user-pass-chk">비밀번호 체크</label>
                         <br/>
                         <Input name="user-password-chk" type="password" value={passwordCheck} required onChange={onChangePasswordCheck} />
+                        {passwordError && <div style={{color:'red'}}>비밀번호가 일치하지 않습니다.</div>}
                     </div>
                     <div>
-                        <Checkbox name="user-term" value={term} onChange={onChangeTerm}>제로초 말을 잘 들을 것을 동의합니다.</Checkbox>
+                        <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>제로초 말을 잘 들을 것을 동의합니다.</Checkbox>
+                        {termError && <div style={{color:'red'}}>약관에 동의하셔야 합니다.</div>}
                     </div>
-                    <div>
+                    <div style={{marginTop: 10}}>
                         <Button type="primary" htmlType="submit">가입하기</Button>
                     </div>
                 </Form>
