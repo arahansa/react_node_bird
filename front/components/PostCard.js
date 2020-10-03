e80e6facd16478df381b9fc82d68cac35849ce8d
arahansa <arahansa@naver.com>
2020-10-03 22:06:27 +0900

    3-3 redux dev tools introduce

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -33,7 +33,7 @@ PostCard.propTypes = {
         User: PropTypes.object,
         content: PropTypes.string,
         img: PropTypes.string,
-        createdAt: PropTypes.object,
+        createdAt: PropTypes.string,
     }),
 };
 
