4cc9f46de1babbf391a654eebc2c6029735f4338
arahansa <arahansa@naver.com>
2020-10-13 10:36:00 +0900

    4-10 react redux part

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,7 +1,15 @@
 import {
   all, fork, takeEvery, takeLatest, call, put, delay,
 } from 'redux-saga/effects';
-import { LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_IN_FAILURE } from '../reducers/user';
+import {
+  LOG_IN_REQUEST,
+  LOG_IN_SUCCESS,
+  LOG_IN_FAILURE,
+  SIGN_UP_REQUEST,
+  SIGN_UP_SUCCESS,
+  SIGN_UP_FAILURE
+} from '../reducers/user';
+import axios from "axios";
 
 const HELLO_SAGA = 'HELLO_SAGA';
 
@@ -42,6 +50,28 @@ function* hello() {
 
 function* watchHello2() {
   yield takeLatest(HELLO_SAGA, hello);
+}
+
+function signUpApi(){
+  return axios.post("/login");
+}
+
+function* signup(){
+  try{
+    yield call(signUpApi);
+    yield put({
+      type: SIGN_UP_SUCCESS,
+    })
+  }catch (e){
+    console.error(e);
+    yield put({
+      type: SIGN_UP_FAILURE
+    })
+  }
+}
+
+function* watchSignUp(){
+  yield takeEvery(SIGN_UP_REQUEST, signup);
 }
 
 export default function* userSaga() {
